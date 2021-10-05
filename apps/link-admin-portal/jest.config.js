5455c843411fdc676faf476267f440f125f4403e
Amanuel Tadesse <amanuel0000@gmail.com>
2021-10-05 09:39:27 -0700

    initial commit

---
+++ apps/link-admin-portal/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'link-admin-portal',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/link-admin-portal',
+};
